880281e02cd9833bc77db65476ba966accbb6212
Ivan <hichigo91@gmail.com>
2017-01-17 17:34:58 +0300

    add structures hero file

---
+++ Source/IslandOfTheVoid/Structures/Hero/HeroStructures.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "HeroStructures.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class ISLANDOFTHEVOID_API UHeroStructures : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
