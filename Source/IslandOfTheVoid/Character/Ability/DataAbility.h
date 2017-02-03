65c4c345a8f42dd5e033ae2277c3dedecf7c44f3
Ivan <hichigo91@gmail.com>
2017-02-03 13:32:42 +0300

    add comment to data ability

--- Source/IslandOfTheVoid/Character/Ability/DataAbility.h
+++ Source/IslandOfTheVoid/Character/Ability/DataAbility.h
@@ -18,21 +18,27 @@ public:
 	
 public:
 
+	/** Ability information */
 	UPROPERTY(EditAnywhere)
 	FAbility AbilityInfo;
 
+	/** Next ability list */
 	UPROPERTY(EditAnywhere)
 	TArray<UDataAbility*> NextAbility;
 
+	/** Previous ability list */
 	UPROPERTY(EditAnywhere)
 	TArray<UDataAbility*> PreviousAbility;
 
+	/** Return current ability info */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Hero|AbilityInfo")
 	FORCEINLINE FAbility GetAbilityInfo() { return AbilityInfo; }
 
+	/** Return next ability list */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Hero|AbilityInfo")
 	FORCEINLINE TArray<UDataAbility*> GetNextAbilites() { return NextAbility; }
 
+	/** Return previous ability list */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Hero|AbilityInfo")
 	FORCEINLINE TArray<UDataAbility*> GetPreviousAbilites() { return PreviousAbility; }
 
