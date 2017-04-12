cb7c45bf29a67257470315c116ce1b57e46f2002
hichigo <hichigo91@gmail.com>
2017-04-12 17:12:42 +0300

    created ai perception in aicontroller base

--- Source/IslandOfTheVoid/Enemy/AIController_Base.cpp
+++ Source/IslandOfTheVoid/Enemy/AIController_Base.cpp
@@ -4,5 +4,41 @@
 #include "AIController_Base.h"
 
 
+AAIController_Base::AAIController_Base(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	AIPerception = ObjectInitializer.CreateDefaultSubobject<UAIPerceptionComponent>(this, TEXT("AIPerception"));
+	
+	SightConfig = ObjectInitializer.CreateDefaultSubobject<UAISenseConfig_Sight>(this, TEXT("SightConfig"));
+	
+	AIPerception->ConfigureSense(*SightConfig);
 
+	AIPerception->SetDominantSense(SightConfig->GetSenseImplementation());
+
+	SightConfig->SightRadius = 1250.0f;
+	SightConfig->LoseSightRadius = 1300.0f;
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+	
+	//Sence.BindUFunction(this, "OnPerceptionUpdated");
+
+	AIPerception->OnPerceptionUpdated.AddDynamic(this, &AAIController_Base::OnPerceptionUpdated);
+		
+		//->OnTargetPerceptionUpdated.//__Internal_AddDynamic(this, &AAIController_Base::OnPerceptionUpdated);
+}
+
+void AAIController_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Pawn = Cast<AAI_Base>(GetPawn());
+
+	MainAction = Pawn->MainStats.MainAction;
+
+}
+
+void AAIController_Base::OnPerceptionUpdated(TArray<AActor*> Actors)
+{
+}
 
--- Source/IslandOfTheVoid/Enemy/AIController_Base.h
+++ Source/IslandOfTheVoid/Enemy/AIController_Base.h
@@ -3,6 +3,9 @@
 #pragma once
 
 #include "AIController.h"
+#include "Enemy/AI_Base.h"
+#include "Runtime/AIModule/Classes/Perception/AIPerceptionComponent.h"
+#include "Runtime/AIModule/Classes/Perception/AISenseConfig_Sight.h"
 #include "AIController_Base.generated.h"
 
 /**
@@ -12,8 +15,22 @@ UCLASS()
 class ISLANDOFTHEVOID_API AAIController_Base : public AAIController
 {
 	GENERATED_BODY()
+
+	AAIController_Base(const FObjectInitializer& ObjectInitializer);
+
+public:
+	virtual void BeginPlay() override;
+
+private:
+	AAI_Base *Pawn;
 	
+	EActionEnemy MainAction;
 	
-	
-	
+	UAIPerceptionComponent *AIPerception;
+
+	UAISenseConfig_Sight* SightConfig;
+
+	//TScriptDelegate<> Sence;
+
+	void OnPerceptionUpdated(TArray<AActor*> Actors);
 };
